e7c06c92c0ffae3343bba6eb36b2dbd0d21ef7df
Luis Fernando Nunes <80663139+fernando-nunes21@users.noreply.github.com>
2022-07-07 19:45:04 -0300

    create: start spring setup

---
+++ src/main/groovy/com/kakashi/projetokakashi/ProjetoKakashiApplication.groovy
@@ -0,0 +1,13 @@
+package com.kakashi.projetokakashi
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ProjetoKakashiApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(ProjetoKakashiApplication, args)
+	}
+
+}
---
+++ src/test/groovy/com/kakashi/projetokakashi/ProjetoKakashiApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.kakashi.projetokakashi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProjetoKakashiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
